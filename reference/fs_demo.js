4ec7447f4ee655c55c62b7df27da61697d5c597b
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-18 22:46:12 +0800

    adding modified fs_demo.js and os_demo.js, deleted reference/test/hello.txt and new reference/test/helloword.txt file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,20 +8,20 @@ const path = require('path');
 // });
 
 // Create and write to file
-fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),
-    'Hello World!',
-    err => {
-        if (err) throw err;
-        console.log('File written to...');
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),
+//     'Hello World!',
+//     err => {
+//         if (err) throw err;
+//         console.log('File written to...');
 
-        // File append
-        fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),
-            ' I love Node.js ',
-            err => {
-                if (err) throw err;
-                console.log('File written to...');
-            });
-    });
+//         // File append
+//         fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),
+//             ' I love Node.js ',
+//             err => {
+//                 if (err) throw err;
+//                 console.log('File written to...');
+//             });
+//     });
 
 // WriteFile function
 // fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),
@@ -30,3 +30,16 @@ fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),
 //         if (err) throw err;
 //         console.log('File written to...');
 //     });
+
+// Read file
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+// Rename file
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test',
+    'helloworld.txt'), err => {
+        if (err) throw err;
+        console.log('File renamed...');
+    });
