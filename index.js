b19508e2b75b4958865959c076e2708b0e3ebcf1
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-20 04:42:32 +0800

    modified:   index.js         new file:   logger.js         modified:   package-lock.json         modified:   package.json         new file:   reference/event_demo.js         new file:   reference/http_demo.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const Person = require('./person');
-// import Person from './person';
+const Logger = require('./logger');
 
-const person1 = new Person('John Doe', 30);
+const logger = new Logger();
 
-person1.greeting();
+logger.on('message', data => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
